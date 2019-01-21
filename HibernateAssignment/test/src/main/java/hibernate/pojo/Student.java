35621d975bec5c3fd63eb7276a2eff2ea6ee362d
Akshath S K <akshath.sk@accoliteindia.com>
2019-01-21 18:29:32 +0530

    seventh commit

---
+++ HibernateAssignment/test/src/main/java/hibernate/pojo/Student.java
@@ -0,0 +1,75 @@
+package hibernate.pojo;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="student")
+public class Student {
+
+	@Id
+	@Column(name="studentId")
+	public Integer studentId;
+
+	@Column(name="firstName")
+	public String firstName;
+	
+	@Column(name="lastName")
+	public String lastName;
+
+	@Column(name="studentAge")
+	public Integer studentAge;
+
+	public Integer getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(Integer studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Integer getStudentAge() {
+		return studentAge;
+	}
+
+	public void setStudentAge(Integer studentAge) {
+		this.studentAge = studentAge;
+	}
+
+	
+	 @OneToMany(mappedBy="studentId", fetch=FetchType.EAGER) public List<Course> courses;
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+}
